625e3c962768d83ef3c31a335a8f83a580344ae4
ricardo-xavier <ricardo.costa.xavier@gmail.com>
2020-05-10 19:43:59 -0300

    unificação dos métodos

--- radarws/src/main/java/radarws/Service.java
+++ radarws/src/main/java/radarws/Service.java
@@ -6,7 +6,6 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -17,36 +16,26 @@ public class Service {
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	public String version() {
-		return "radarws v0.1.0(10/05/2020)";
+		return "radarws v0.2.0(10/05/2020)";
 	}
 	
 	@POST
 	@Path("/atualiza")
 	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
 	public String atualiza(String json) {
 		System.out.println(new Date() + " radarws atualiza");
 		System.out.println(json);
 		Gson gson = new Gson();
 		Individuo individuo = gson.fromJson(json, Individuo.class);					
 		IndividuoDao.atualiza(individuo);
-		return "ok";
-	}
-	
-
-	@GET
-	@Path("/vizinhanca/{id}")
-	@Produces(MediaType.APPLICATION_JSON)
-	public String vizinhanca(@PathParam("id") String id) {
-		System.out.println(new Date() + " radarws vizinhanca " + id);
 		Vizinhanca vizinhanca = new Vizinhanca();
-		Individuo individuo = IndividuoDao.recupera(id);
-		if (individuo != null) {
-			vizinhanca.setVizinhos(IndividuoDao.getVizinhanca(individuo));
-		}
-		Gson gson = new Gson();
+		vizinhanca.setVizinhos(IndividuoDao.getVizinhanca(individuo));
+		gson = new Gson();
 		return gson.toJson(vizinhanca);		
 	}
 
 }
 
-// 10/05/2020 - 0.1.0 - versão inicial
+//10/05/2020 - 0.1.0 - versão inicial
+//10/05/2020 - 0.2.0 - unificação dos métodos
